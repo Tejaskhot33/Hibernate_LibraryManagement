088238b65bb0691eb9a02f4579a0107efa2ce0eb
Tejaskhot33 <113814257+Tejaskhot33@users.noreply.github.com>
2023-10-10 11:51:04 +0530

    update
    update

---
+++ src/main/java/MainController.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+import org.hibernate.internal.build.AllowSysOut;
+
+public class MainController {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		Library library = new Library();
+		Library_Dao dao = new Library_Dao();
+
+		System.out.println(
+				" \n 1.Add a New Book \n 2.Search by Book name \n 3.Search by Author Name  \n 4.Check book availability \n 5.Show All Book \n 6.");
+		int choice = scanner.nextInt();
+
+		switch (choice) {
+		case 1:
+			System.out.println("Enter the id");
+			library.setId(scanner.nextInt());
+
+			System.out.println("Enter the book name");
+			library.setBook(scanner.next());
+
+			System.out.println("Enter the author name");
+			library.setAuthor(scanner.next());
+
+			System.out.println("Enter the price");
+			library.setPrice(scanner.nextDouble());
+
+			System.out.println("Enter the count of books");
+			library.setCount(scanner.nextInt());
+
+			System.out.println("Enter the type of book");
+			library.setType(scanner.next());
+
+			dao.saveBook(library);
+
+			break;
+
+		case 2:
+			System.out.println("Enter the book id");
+			int id = scanner.nextInt();
+
+			System.out.println("Enter the book name");
+			library.setBook(scanner.next());
+
+//			dao.searchAuthor(library);
+
+			dao.searchBook(id,library.getBook());
+			break;
+
+		default:
+			break;
+		}
+	}
+
+}
